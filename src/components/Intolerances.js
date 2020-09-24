cbb1bee91496ded1f97179bd856b5e0a189eb3e8
James Fraser <jimbob4t@virginmedia.com>
2020-09-24 11:46:40 +0100

    Styling intolerances list

--- src/components/Intolerances.js
+++ src/components/Intolerances.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Menu, MenuOptions, MenuOption, MenuTrigger } from 'react-native-popup-menu';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, ScrollView } from 'react-native';
 
 const Intolerances = ({ intolerances, setIntolerances }) => {
 	const removeIntolerance = (intolerance) => {
@@ -12,126 +12,129 @@ const Intolerances = ({ intolerances, setIntolerances }) => {
 
 	return (
 		<View style={styles.intolerances_container}>
-			<Menu>
-				<MenuTrigger style={styles.intolerance_button}>
-					<Text style={styles.intolerance_button_text}>Add Intolerances</Text>
-				</MenuTrigger>
-				<MenuOptions style={styles.intolerance_options} customStyles={intolerance_dropdown}>
-					<MenuOption
-						text="Dairy"
-						customStyles={intolerance_option}
-						onSelect={() => {
-							const newSelectedIntolerances = intolerances.concat('Dairy');
-							setIntolerances(newSelectedIntolerances);
-						}}
-					/>
-					<MenuOption
-						text="Egg"
-						customStyles={intolerance_option}
-						onSelect={() => {
-							const newSelectedIntolerances = intolerances.concat('Egg');
-							setIntolerances(newSelectedIntolerances);
-						}}
-					/>
-					<MenuOption
-						text="Gluten"
-						customStyles={intolerance_option}
-						onSelect={() => {
-							const newSelectedIntolerances = intolerances.concat('Gluten');
-							setIntolerances(newSelectedIntolerances);
-						}}
-					/>
-					<MenuOption
-						text="Grain"
-						customStyles={intolerance_option}
-						onSelect={() => {
-							const newSelectedIntolerances = intolerances.concat('Grain');
-							setIntolerances(newSelectedIntolerances);
-						}}
-					/>
-					<MenuOption
-						text="Peanut"
-						customStyles={intolerance_option}
-						onSelect={() => {
-							const newSelectedIntolerances = intolerances.concat('Peanut');
-							setIntolerances(newSelectedIntolerances);
-						}}
-					/>
-					<MenuOption
-						text="Seafood"
-						customStyles={intolerance_option}
-						onSelect={() => {
-							const newSelectedIntolerances = intolerances.concat('Seafood');
-							setIntolerances(newSelectedIntolerances);
-						}}
-					/>
-					<MenuOption
-						text="Sesame"
-						customStyles={intolerance_option}
-						onSelect={() => {
-							const newSelectedIntolerances = intolerances.concat('Sesame');
-							setIntolerances(newSelectedIntolerances);
-						}}
-					/>
-					<MenuOption
-						text="Shellfish"
-						customStyles={intolerance_option}
-						onSelect={() => {
-							const newSelectedIntolerances = intolerances.concat('Shellfish');
-							setIntolerances(newSelectedIntolerances);
-						}}
-					/>
-					<MenuOption
-						text="Soy"
-						customStyles={intolerance_option}
-						onSelect={() => {
-							const newSelectedIntolerances = intolerances.concat('Soy');
-							setIntolerances(newSelectedIntolerances);
-						}}
-					/>
-					<MenuOption
-						text="Sulfite"
-						customStyles={intolerance_option}
-						onSelect={() => {
-							const newSelectedIntolerances = intolerances.concat('Sulfite');
-							setIntolerances(newSelectedIntolerances);
-						}}
-					/>
-					<MenuOption
-						text="Tree Nut"
-						customStyles={intolerance_option}
-						onSelect={() => {
-							const newSelectedIntolerances = intolerances.concat('Tree Nut');
-							setIntolerances(newSelectedIntolerances);
-						}}
-					/>
-					<MenuOption
-						text="Wheat"
-						customStyles={intolerance_option}
-						onSelect={() => {
-							const newSelectedIntolerances = intolerances.concat('Wheat');
-							setIntolerances(newSelectedIntolerances);
-						}}
-					/>
-				</MenuOptions>
-			</Menu>
+			<ScrollView>
+				<Menu>
+					<MenuTrigger style={styles.intolerance_button}>
+						<Text style={styles.intolerance_button_text}>Add Intolerances</Text>
+					</MenuTrigger>
 
-			<FlatList
-				data={intolerances}
-				// style={styles.flatlist}
-				// ListEmptyComponent={<Text>hi</Text>}
-				renderItem={({ item, index }) => (
-					<View keyExtractor={index.toString()} style={styles.intolerance_select}>
-						<Text style={styles.intolerance_name}>{item}</Text>
-						<TouchableOpacity
-							style={styles.remove_intolerance_button}
-							onPress={() => removeIntolerance(item)}
-						>
-							<Text style={styles.remove_intolerance_button_text}>X</Text>
-						</TouchableOpacity>
-					</View>
-				)}
-			/>
+					<MenuOptions style={styles.intolerance_options} customStyles={intolerance_dropdown}>
+						<MenuOption
+							text="Dairy"
+							customStyles={intolerance_option}
+							onSelect={() => {
+								const newSelectedIntolerances = intolerances.concat('Dairy');
+								setIntolerances(newSelectedIntolerances);
+							}}
+						/>
+						<MenuOption
+							text="Egg"
+							customStyles={intolerance_option}
+							onSelect={() => {
+								const newSelectedIntolerances = intolerances.concat('Egg');
+								setIntolerances(newSelectedIntolerances);
+							}}
+						/>
+						<MenuOption
+							text="Gluten"
+							customStyles={intolerance_option}
+							onSelect={() => {
+								const newSelectedIntolerances = intolerances.concat('Gluten');
+								setIntolerances(newSelectedIntolerances);
+							}}
+						/>
+						<MenuOption
+							text="Grain"
+							customStyles={intolerance_option}
+							onSelect={() => {
+								const newSelectedIntolerances = intolerances.concat('Grain');
+								setIntolerances(newSelectedIntolerances);
+							}}
+						/>
+						<MenuOption
+							text="Peanut"
+							customStyles={intolerance_option}
+							onSelect={() => {
+								const newSelectedIntolerances = intolerances.concat('Peanut');
+								setIntolerances(newSelectedIntolerances);
+							}}
+						/>
+						<MenuOption
+							text="Seafood"
+							customStyles={intolerance_option}
+							onSelect={() => {
+								const newSelectedIntolerances = intolerances.concat('Seafood');
+								setIntolerances(newSelectedIntolerances);
+							}}
+						/>
+						<MenuOption
+							text="Sesame"
+							customStyles={intolerance_option}
+							onSelect={() => {
+								const newSelectedIntolerances = intolerances.concat('Sesame');
+								setIntolerances(newSelectedIntolerances);
+							}}
+						/>
+						<MenuOption
+							text="Shellfish"
+							customStyles={intolerance_option}
+							onSelect={() => {
+								const newSelectedIntolerances = intolerances.concat('Shellfish');
+								setIntolerances(newSelectedIntolerances);
+							}}
+						/>
+						<MenuOption
+							text="Soy"
+							customStyles={intolerance_option}
+							onSelect={() => {
+								const newSelectedIntolerances = intolerances.concat('Soy');
+								setIntolerances(newSelectedIntolerances);
+							}}
+						/>
+						<MenuOption
+							text="Sulfite"
+							customStyles={intolerance_option}
+							onSelect={() => {
+								const newSelectedIntolerances = intolerances.concat('Sulfite');
+								setIntolerances(newSelectedIntolerances);
+							}}
+						/>
+						<MenuOption
+							text="Tree Nut"
+							customStyles={intolerance_option}
+							onSelect={() => {
+								const newSelectedIntolerances = intolerances.concat('Tree Nut');
+								setIntolerances(newSelectedIntolerances);
+							}}
+						/>
+						<MenuOption
+							text="Wheat"
+							customStyles={intolerance_option}
+							onSelect={() => {
+								const newSelectedIntolerances = intolerances.concat('Wheat');
+								setIntolerances(newSelectedIntolerances);
+							}}
+						/>
+					</MenuOptions>
+				</Menu>
+
+				<FlatList
+					data={intolerances}
+					// style={styles.flatlist}
+					// ListEmptyComponent={<Text>hi</Text>}
+					renderItem={({ item, index }) => (
+						<View keyExtractor={index.toString()} style={styles.intolerance_select}>
+							<Text style={styles.intolerance_name}>{item}</Text>
+							<TouchableOpacity
+								style={styles.remove_intolerance_button}
+								onPress={() => removeIntolerance(item)}
+							>
+								<Text style={styles.remove_intolerance_button_text}>X</Text>
+							</TouchableOpacity>
+						</View>
+					)}
+				/>
+			</ScrollView>
 		</View>
 	);
 };
